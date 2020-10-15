fd0b6b90118f8e942cab9749bb931d1afb7f70df
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-15 15:23:49 +0100

    Refactor to use sessions

--- app.rb
+++ app.rb
@@ -2,14 +2,24 @@ require 'sinatra'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @Player1_Name = params[:Player1_Name]
-    @Player2_Name = params[:Player2_Name]
-    erb(:names)
+    session[:Player1_Name] = params[:Player1_Name]
+    session[:Player2_Name] = params[:Player2_Name]
+    redirect '/play'
+  end
+
+
+  get '/play' do
+    @Player1_Name = session[:Player1_Name]
+    @Player2_Name = session[:Player2_Name]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
