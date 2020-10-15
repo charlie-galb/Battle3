3fabccea35a5dddb6ec2913a33b834e92894f1fa
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-15 16:06:34 +0100

    Refeactored tests - added web_helpers

--- spec/features/view_names_spec.rb
+++ spec/features/view_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Visible names' do
   scenario 'Can enter player names in a form and see them on screen' do
-    visit("/")
-    fill_in('Player1_Name', with: 'Beca')
-    fill_in('Player2_Name', with: 'Charlie')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Beca'
     expect(page).to have_content 'Charlie'
   end
--- spec/features/view_player2_score_spec.rb
+++ spec/features/view_player2_score_spec.rb
@@ -1,9 +1,6 @@
 feature 'view score' do
   scenario 'Can view the opponent score' do
-    visit("/")
-    fill_in('Player1_Name', with: 'Beca')
-    fill_in('Player2_Name', with: 'Charlie')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('20/60 HP').twice
   end
 end
