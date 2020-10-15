6b1cbf50d6c7bfd4902859798b4688cc2b29c685
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-15 17:10:44 +0100

    Player1 can attack player 2 and reduce his/her HP by 10

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require './actions.rb'
 
 class Battle < Sinatra::Base
 
@@ -12,17 +13,32 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:Player1_Name] = params[:Player1_Name]
     session[:Player2_Name] = params[:Player2_Name]
+    session[:Player1_HP] = "60"
+    session[:Player2_HP] = "60"
     redirect '/play'
   end
-
 
   get '/play' do
     @Player1_Name = session[:Player1_Name]
     @Player2_Name = session[:Player2_Name]
-    @Player1_HP = "20"
-    @Player2_HP = "20"
+    @Player1_HP = session[:Player1_HP]
+    @Player2_HP = session[:Player2_HP]
     erb(:play)
   end
+
+  # get '/attack' do
+  #   @Player1_Name = session[:Player1_Name]
+  #   @Player2_Name = session[:Player2_Name]
+  #   erb(:attack)
+  # end
+
+  post "/attack" do
+    session[:Player2_HP]
+    session[:Player2_HP] = attack(session[:Player2_HP])
+    redirect "/play"
+  end
+
+
 
 
   # start the server if ruby file executed directly
--- spec/features/view_player2_score_spec.rb
+++ spec/features/view_player2_score_spec.rb
@@ -1,6 +1,6 @@
 feature 'view score' do
   scenario 'Can view the opponent score' do
     sign_in_and_play
-    expect(page).to have_content('20/60 HP').twice
+    expect(page).to have_content('60/60 HP').twice
   end
 end
